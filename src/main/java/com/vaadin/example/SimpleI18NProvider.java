3fbcc72e29b1baeaa22f18286bf7dae767e0f059
Shadi Khani <shadi@vaadin.com>
2020-05-29 13:40:09 +0300

    Change Map into properties files for languages

--- src/main/java/com/vaadin/example/SimpleI18NProvider.java
+++ src/main/java/com/vaadin/example/SimpleI18NProvider.java
@@ -1,59 +1,58 @@
 package com.vaadin.example;
 
+import com.vaadin.flow.i18n.I18NProvider;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.MessageFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-
-import org.springframework.stereotype.Component;
-
-import com.vaadin.flow.i18n.I18NProvider;
+import java.util.Properties;
 
 /**
  * Simple implementation of {@link I18NProvider}.
  */
 @Component
 public class SimpleI18NProvider implements I18NProvider {
-
+    private static final List<Locale> PROVIDED_LOCALES = Collections
+            .unmodifiableList(Arrays.asList(Locales.ENGLISH, Locales.FRENCH,
+                    Locales.FINNISH));
     private static Map<Locale, Map<String, String>> localeMap = initMap();
+    private static final String PROPERTIES_FILE = "src/main/resources/labelsbundle_{0}.properties";
 
     private static Map<Locale, Map<String, String>> initMap() {
         Map<Locale, Map<String, String>> localeMap = new HashMap<>();
 
-        localeMap.put(Locales.FINNISH, new HashMap<>());
-        localeMap.get(Locales.FINNISH).put("helloButton", "Sano Hei");
-        localeMap.get(Locales.FINNISH).put("yourName", "Sinun nimesi");
-        localeMap.get(Locales.FINNISH).put("selectLanguage", "Valitse kieli");
-        localeMap.get(Locales.FINNISH).put("greeting", "Hei {0}!");
-        localeMap.get(Locales.FINNISH).put("anonymousGreeting",
-                "Hei tuntematon!");
-
-        localeMap.put(Locales.ENGLISH, new HashMap<>());
-        localeMap.get(Locales.ENGLISH).put("helloButton", "Say hello");
-        localeMap.get(Locales.ENGLISH).put("yourName", "Your name");
-        localeMap.get(Locales.ENGLISH).put("selectLanguage",
-                "Select a language");
-        localeMap.get(Locales.ENGLISH).put("greeting", "Hello {0}!");
-        localeMap.get(Locales.ENGLISH).put("anonymousGreeting",
-                "Hello anonymous!");
-
-        localeMap.put(Locales.FRENCH, new HashMap<>());
-        localeMap.get(Locales.FRENCH).put("helloButton", "Dire bonjoure");
-        localeMap.get(Locales.FRENCH).put("yourName", "Votre nom");
-        localeMap.get(Locales.FRENCH).put("selectLanguage",
-                "Sélectionnez une langue");
-        localeMap.get(Locales.FRENCH).put("greeting", "Bonjoure {0}!");
-        localeMap.get(Locales.FRENCH).put("anonymousGreeting",
-                "Bonjour anonyme!");
-
+        for (Locale locale : PROVIDED_LOCALES) {
+            String fileName = MessageFormat.format(PROPERTIES_FILE,
+                    locale.getLanguage().toLowerCase());
+            localeMap.put(locale, new HashMap<>());
+            try (FileInputStream fileInputStream = new FileInputStream(
+                    fileName)) {
+                InputStreamReader inputStreamReader = new InputStreamReader(
+                        fileInputStream, "UTF-8");
+                Properties properties = new Properties();
+                properties.load(inputStreamReader);
+                for (String key : properties.stringPropertyNames()) {
+                    String value = properties.getProperty(key);
+                    localeMap.get(locale).put(key, value);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return localeMap;
     }
 
     @Override
     public List<Locale> getProvidedLocales() {
-        return new ArrayList<>(localeMap.keySet());
+        return PROVIDED_LOCALES;
     }
 
     @Override
